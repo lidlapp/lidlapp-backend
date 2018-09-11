170233663549bae0985645ad4fb73809a9feccaf
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-09-11 10:54:19 +0200

    Make user a JPA entity

--- src/main/java/lidlapp/models/User.java
+++ src/main/java/lidlapp/models/User.java
@@ -1,10 +1,15 @@
 package lidlapp.models;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class User {
+    private User() {
+    }
+
     @Id
     @GeneratedValue
     private Long id;
@@ -18,4 +23,44 @@ public class User {
     private String iban;
 
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
