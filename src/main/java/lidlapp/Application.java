e53f5499ed4dd055b3ad1ff8e6f8d42c28df11b0
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-09-12 08:54:38 +0200

    Get Spring Boot application booted up

--- src/main/java/lidlapp/Application.java
+++ src/main/java/lidlapp/Application.java
@@ -1,11 +1,24 @@
 package lidlapp;
 
+import com.google.common.collect.Iterators;
+import lidlapp.repos.CourierRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(CourierRepository courierRepository) {
+        return args -> {
+            var couriers = courierRepository.findAll();
+            int count = Iterators.size(couriers.iterator());
+            System.out.printf("Found %d couriers.%n", count);
+        };
     }
 }
