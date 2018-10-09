0ff859d3c9cffbb5c188235109080fcef1c6ac95
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-10-09 12:46:05 +0200

    Allow frames in h2-console

--- src/main/java/lidlapp/config/WebSecurity.java
+++ src/main/java/lidlapp/config/WebSecurity.java
@@ -21,6 +21,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .headers().frameOptions().disable().and()
                 .httpBasic().disable()
                 .csrf().and()
                 .authorizeRequests()
